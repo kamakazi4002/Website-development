a5192995aa6c9da5e181c035abcf67594c656a8c
Callum Purchase <cgwp2004@gmail.com>
2024-12-29 21:51:14 +0000

    updated code

---
+++ Werbsite development/passcheck.js
@@ -0,0 +1,21 @@
+﻿function passcheck() { //This declares the fucntion to check if the password is correct 
+
+
+    //sets password
+    var code = "Password1";
+
+
+    //Captures password input from the user, using the textbox in the form
+    var input = document.getElementById("userpass").value;
+
+
+    if (input == code) {
+        //Send user to admin page
+        document.getElementById("adminlogin").action = "sysadmin.html";
+
+    } else {
+        document.getElementById("adminlogin").action = "notadmin.html";
+        //tell user incorrect password, send to invalid page
+
+    }
+}
